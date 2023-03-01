5b2489261aaa486740a92ac8f44bd02f252fc4bf
wilberforce001 <kenmisik@gmail.com>
2023-03-01 12:01:54 +0300

    Changed the text

--- app/src/main/java/com/example/mygreetingcard/MainActivity.kt
+++ app/src/main/java/com/example/mygreetingcard/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hi, my name is $name!")
 }
 
 @Preview(showBackground = true)
